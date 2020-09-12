3f046e1a45f526855bcb8c8efc5a0e5e460baaf2
Alan Grover <awgrover@gmail.com>
2020-09-12 14:25:33 -0400

    Revert "cleanup and start subdirectories"
    This reverts commit fecae9352fece7689e3b9d674a17db193138eab8.

---
+++ dross/From older CPX/Examples/warn if overturned.py
@@ -0,0 +1,14 @@
+from adafruit_circuitplayground.express import cpx
+import time
+
+cpx.pixels.brightness = 0.6
+
+while True:
+    x, y, z = cpx.acceleration
+    print((x, y, z))
+    time.sleep(0.1)
+    if z < -2:
+        cpx.pixels.fill((0,0,255))
+        cpx.play_tone(1500, 0.25)
+    else:
+        cpx.pixels.fill((0,0,50))
