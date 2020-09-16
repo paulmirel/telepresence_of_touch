e1a8da27e1288ea1aa336a1dfbf3540a721b68c5
Alan Grover <awgrover@gmail.com>
2020-09-16 17:28:26 -0400

    using every

--- circuit-playground-express/telepresence_of_touch_code/code.py
+++ circuit-playground-express/telepresence_of_touch_code/code.py
@@ -5,39 +5,46 @@
 from adafruit_circuitplayground import cp
 import time
 
+import sys
+sys.path.append('%PROJECT/lib')
+from every import Every
+
 cp.pixels.brightness = 0.6
 
 light_color = ( 255, 0, 0 )
 OFF = ( 0, 0, 0 )
 
+every_tenth = Every(0.1)
+
 while True:
-    output = "output"
-    # touch near pixel 1, between A4 and A5
-    if cp.touch_A4 or cp.touch_A5: # use -or- for greater sensitivity to touch, use -and- for more localized sensitivity to touch
-        cp.pixels[ 1 ] = light_color
-        output = output + " 1"
-    else:
-        cp.pixels[ 1 ] = OFF
+    if every_tenth():
+        output = ""
+        # touch near pixel 1, between A4 and A5
+        if cp.touch_A4 or cp.touch_A5: # use -or- for greater sensitivity to touch, use -and- for more localized sensitivity to touch
+            cp.pixels[ 1 ] = light_color
+            output = output + " 1"
+        else:
+            cp.pixels[ 1 ] = OFF
 
-    # touch near pixel 3, between A6 and A7
-    if cp.touch_A6 or cp.touch_A7: # use -or- for greater sensitivity to touch, use -and- for more localized sensitivity to touch
-        cp.pixels[ 3 ] = light_color
-        output = output + " 3"
-    else:
-        cp.pixels[ 3 ] = OFF
+        # touch near pixel 3, between A6 and A7
+        if cp.touch_A6 or cp.touch_A7: # use -or- for greater sensitivity to touch, use -and- for more localized sensitivity to touch
+            cp.pixels[ 3 ] = light_color
+            output = output + " 3"
+        else:
+            cp.pixels[ 3 ] = OFF
 
-    # touch near pixel 6, between A0 and A1
-    if cp.touch_A1: # cp.touch_A0 is not available in the cp library
-        cp.pixels[ 6 ] = light_color
-        output = output + " 6"
-    else:
-        cp.pixels[ 6 ] = OFF
+        # touch near pixel 6, between A0 and A1
+        if cp.touch_A1: # cp.touch_A0 is not available in the cp library
+            cp.pixels[ 6 ] = light_color
+            output = output + " 6"
+        else:
+            cp.pixels[ 6 ] = OFF
 
-    # touch near pixel 8, between A2 and A3
-    if cp.touch_A2 or cp.touch_A3: # use -or- for greater sensitivity to touch, use -and- for more localized sensitivity to touch
-        cp.pixels[ 8 ] = light_color
-        output = output + " 8"
-    else:
-        cp.pixels[ 8 ] = OFF
-    print( output )
-    time.sleep( 0.1 )
+        # touch near pixel 8, between A2 and A3
+        if cp.touch_A2 or cp.touch_A3: # use -or- for greater sensitivity to touch, use -and- for more localized sensitivity to touch
+            cp.pixels[ 8 ] = light_color
+            output = output + " 8"
+        else:
+            cp.pixels[ 8 ] = OFF
+        if output != "":
+            print( "output", output )
