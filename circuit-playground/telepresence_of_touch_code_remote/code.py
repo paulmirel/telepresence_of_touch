92dece6babda520bb3c0b41bd47665a39daf7b1d
Alan Grover <awgrover@gmail.com>
2020-09-26 14:47:31 -0400

    workign again, simplified

--- circuit-playground/telepresence_of_touch_code_remote/code.py
+++ circuit-playground/telepresence_of_touch_code_remote/code.py
@@ -19,11 +19,8 @@ from adafruit_circuitplayground import cp
 gc.collect()
 from every import Every
 gc.collect()
-from unrvl_mqtt import UnrvlMQTT
+from mqtt_serial import SerialMQTT
 gc.collect()
-
-# seed the random generator
-random.seed(int(cp.light + cp.temperature * 100 + time.monotonic()*1000));
 
 LastTouchPart = {}
 
@@ -35,6 +32,9 @@ def update_touch(mqtt_message):
     # might be a bunch of entries like
     # { touch" : { 1 : [0,255,0], 2: [255,0,0], 3:[0,0,0] }
     touch_part = {}
+
+    # We use the odd pixels for signalling local-touch
+    # (we'll use the even for remote touch)
 
     # (fixme: what possible ways are there to "factor" this repetition?)
     # touch near pixel 1, between A4 and A5
@@ -76,19 +76,6 @@ def update_touch(mqtt_message):
         LastTouchPart = touch_part # remember for next time
     gc.collect()
 
-def do_touch_message( topic, key, value, full_message ):
-    # value looks like:
-    #   { 1: (0,0,0), 4: (255,0,0), ... }
-    # i.e. which "touch" and the desired color
-    #print("debugmqtt: handled",topic,"|",key,":",value," in ",full_message)
-    remote_to_local = [ 0, 0, 2, 5, 7 ] # map of touch # to local led
-    for which, color in value.items():
-        print('remote', which, color)
-        if color == (0,0,0):
-            cp.pixels[ remote_to_local[which] ] = OFF
-        else:
-            cp.pixels[ remote_to_local[which] ] = RemoteColor # ignore their color
-
 def random_color():
     # pick a random color, but near the rim of the color wheel (high saturation/value)
     rg_or_b = random.uniform(1,3.99);
@@ -106,22 +93,62 @@ def random_color():
         blue = 0
     return (red,green,blue)
 
+def do_subscriptions():
+    # when we finally connect, need to subscribe
+    for topic in MQTTSubscribe:
+        mqtt.subscribe(topic)
+
+def handle_mqtt_message():
+    # See if there is an incomming message
+    # react to it
+
+    topic, mqtt_message = mqtt.receive_message()
+    if not mqtt_message:
+        return # nothing to do this time
+
+    # FIXME: ignore our own message! initials
+
+    print("MSG ", mqtt_message)
+
+    # We could make decisions about what to do based on the topic...
+    
+    # A "touch" message looks like:
+    # { touch" : { 1 : [0,255,0], 2: [255,0,0], 3:[0,0,0] }
+    # i.e. which "touch" and the requested color
+    if "touch" in mqtt_message:
+        value = mqtt_message["touch"]
+
+        # make sure it's a dict!
+        if ( not isinstance(value, dict) ):
+            print("debugmqtt: 'touch' wasn't an dict:",value)
+            return
+
+        print("Touch! ")
+        remote_to_local = [ 0, 2, 4, 6, 8 ] # map of touch number to local led number (unused)
+
+        for which, color in value.items():
+            print('remote', which, 'to', remote_to_local[which],color)
+            if color == (0,0,0):
+                cp.pixels[ remote_to_local[which] ] = OFF
+            elif isinstance(color,tuple) or isinstance(color,list):
+                cp.pixels[ remote_to_local[which] ] = RemoteColor # ignore their color
+            else:
+                print("debugmqtt: expected the 'color' to be a tuple/list, saw", color.__class__.__name__,color)
+
 ### Startup
 
-# MQTT server & topics
-unrvl_mqtt = UnrvlMQTT("mqtt://localhost:1883")
-# what channels do we want to listen to?
-unrvl_mqtt.subscribe(
-    "unrvl2020/touch",
-    {
-    # what do we want to do with each key:value
-    'touch' : do_touch_message
-    }
-)
-MQTTPublishTo = 'unrvl2020/touch'
+MQTTServer = "mqtt://localhost:1883"
 
-# pick our color, randomly
-RemoteColor = random_color() # edit me! what's your color?
+MQTTPublishTo = 'unrvl2020/touch-everyone'
+MQTTSubscribe = [ 'unrvl2020/touch-everyone' ]
+
+# your initials
+Me='awg'
+
+# pick our color
+# (seed the random generator)
+random.seed(int(cp.light + cp.temperature * 100 + time.monotonic()*1000));
+RemoteColor = random_color() # Random. edit me! what's your color?
 LocalColor = RemoteColor
 print("My color is", RemoteColor);
 
@@ -138,10 +165,16 @@ OFF = ( 0, 0, 0 )
 cp.pixels[0] = RemoteColor;
 
 # MQTT (remote communication)
-unrvl_mqtt.connect()
+# MQTT server & topics
+mqtt = SerialMQTT(MQTTServer)
+mqtt.connect()
+
+# what channels do we want to listen to?
+mqtt.subscribe( "unrvl2020/touch-everyone" )
+# mqtt.subscribe( "unrvl2020/shake-group1" )
 
 # setup event handling
-every_update_remote = Every(3) # how often to send complete state
+every_update_remote = Every(3) # how often to send complete state FIXME: drop?
 
 ### Loop
 print("Free memory before loop",gc.mem_free())
@@ -152,10 +185,11 @@ while True:
         #print("heartbeat free mermory", gc.mem_free(), time.monotonic())
         cp.red_led = not cp.red_led # clever "toggle", aka "blink"
 
+
     # Add key:value that the remote system should know about,
     # e.g. mqtt_message['touch1'] = LocalColor
     # The remote system has to know what to do with each "key"
-    mqtt_message = {} # will be sent if anything changed
+    mqtt_message = {} # filled in and will be sent if anything changed
 
     # we can react locally very fast: 0.01 sec
     if every_update_touch():
@@ -164,11 +198,17 @@ while True:
 
     # if anything to send, do it
     if mqtt_message:
-        unrvl_mqtt.publish(MQTTPublishTo, mqtt_message)
+        mqtt.publish(MQTTPublishTo, mqtt_message)
 
-    # handle mqtt events
-    other_message = unrvl_mqtt.run()
+    # handle mqtt communication
+    other_message, should_reconnect = mqtt.run()
+    # we may have to connect (or re-connect)
+    if should_reconnect:
+        print("## reconnect?", should_reconnect)
+        do_subscriptions()
 
     # just echo any other text that came over the serial
     if other_message:
         print("Not understood:", other_message)
+
+    handle_mqtt_message()
