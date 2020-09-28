facc58755cef1d57ef247b9e275fcbea02c2d0f7
Paul_Mirel <paul.mirel@startmail.com>
2020-09-27 20:14:13 -0400

    modified code to receive input from usb to show blinking light. receive code still hangs waiting for Enter.

--- circuit-playground/other_codes/taking_input_from_usb/code.py
+++ circuit-playground/other_codes/taking_input_from_usb/code.py
@@ -1,11 +1,18 @@
 import supervisor
 import time
+from adafruit_circuitplayground import cp
 
-print( "listening..." )
+cp.pixels.brightness = 0.6
+
+light_color = ( 255, 0, 0 )
+OFF = ( 0, 0, 0 )
+
 
 while True:
-    print( "going on" )
-    time.sleep( 1 )
+    cp.pixels[ 1 ] = light_color
+    time.sleep( 0.5 )
+    cp.pixels[ 1 ] = OFF
+    time.sleep( 0.5 )
     if supervisor.runtime.serial_bytes_available:
         value = input().strip()
         # Sometimes Windows sends an extra (or missing) newline - ignore them
